8ef01bdffa86d51449e287781650ac8a394e3465
Pavithra C R <114286838+pavithracr1111@users.noreply.github.com>
2023-08-16 23:13:32 +0530

    Add files via upload

---
+++ c and c++ basic coding/Natural numbers/c++.cpp
@@ -0,0 +1,61 @@
+/* C++ (For Loop)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int num;
+    cout << "Enter the number: "; cin >> num;
+    
+    for(int i=1;i<=num;i++){
+         cout << i << " ";
+    }
+    
+    return 0;
+
+}
+
+
+C++ (While Loop)
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int num;
+    cout << "Enter the number: "; cin >> num;
+    
+    int i = 1;
+    
+    while(i<=num){
+         cout << i << " ";
+        i++;
+    }
+    
+    return 0;
+
+}
+*/
+
+
+//C++ (Do-While Loop)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int num;
+    cout << "Enter the number: ";
+     cin >> num;
+    
+    int i = 1;
+    
+    do{
+        cout << i << " ";
+        i++;
+    }while(i<=num);
+    
+    return 0;
+}
